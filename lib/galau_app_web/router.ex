12be22b9c85eadfcf2a21d2989ff977a8731f46f
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-11-07 19:57:21 +0700

    Featura Question

--- lib/galau_app_web/router.ex
+++ lib/galau_app_web/router.ex
@@ -17,6 +17,7 @@ defmodule GalauAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/questions", QuestionController
   end
 
   # Other scopes may use custom stacks.
